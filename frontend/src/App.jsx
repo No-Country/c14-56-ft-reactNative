f9d2859171c78c0a2d83459d046dee3a8515a246
LucasFerdal <electroblue25@gmail.com>
2023-10-09 12:46:52 -0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div class="flex justify-center items-center h-screen">
+        <p class="text-center text-5xl text-sky-400">c14-56-ft-reactnative</p>
+      </div>
+    </>
+  )
+}
+
+export default App
