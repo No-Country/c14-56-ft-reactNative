58db7c6864f25dac7995d2b3f89c30f2bbd42a83
Tommyx66 <tomz.ry64@gmail.com>
2023-10-10 18:55:17 -0300

    Subida Componentes Icons

---
+++ frontend/src/Components/Home/Icons/UserIcon/UserIcon.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'font-awesome/css/font-awesome.min.css';
+import '../IconStyles.css';
+
+const UserIcon = () => {
+    return <i className="fa fa-user  white-icon"></i>;
+};
+
+export default UserIcon;
