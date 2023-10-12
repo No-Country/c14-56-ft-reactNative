f32e55347f6f6c9b222be1a49f16106b58d079dc
Tommyx66 <tomz.ry64@gmail.com>
2023-10-12 00:33:49 -0300

    Subida Componente UserList

---
+++ frontend/src/Components/UserList/UserList.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import UserCard from '../UserCard/UserCard';
+import './UserList.css';
+
+const UserList = () => {
+  const users = [
+    { name: 'Usuario 1', username: 'usuario1', avatarUrl: 'url_avatar_1' },
+    { name: 'Usuario 2', username: 'usuario2', avatarUrl: 'url_avatar_2' },
+    { name: 'Usuario 3', username: 'usuario3', avatarUrl: 'url_avatar_3' }
+  ];
+
+  const handleFollowClick = (index) => {
+    // Lógica para seguir al usuario en la posición 'index'
+    console.log('Siguiendo al usuario', users[index].name);
+  };
+
+  return (
+    <div className="user-list">
+      <h2 className='h2-follow'>Follow</h2>
+      {users.map((user, index) => (
+        <UserCard
+          key={index}
+          name={user.name}
+          username={user.username}
+          avatarUrl={user.avatarUrl}
+          onFollowClick={() => handleFollowClick(index)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default UserList;
+
