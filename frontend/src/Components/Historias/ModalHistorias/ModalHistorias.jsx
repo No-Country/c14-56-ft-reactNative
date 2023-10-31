f01d8712c5ca12e22e470007f3940697eeecf5ab
LucasFerdal <electroblue25@gmail.com>
2023-10-31 11:07:40 -0300

    added: search input and some styles

--- frontend/src/Components/Historias/ModalHistorias/ModalHistorias.jsx
+++ frontend/src/Components/Historias/ModalHistorias/ModalHistorias.jsx
@@ -1,45 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './ModalHistorias.css'
 
-import { infoHistorias } from '@HistoriesContainer/InfoHistorias';
-
-const ModalHistorias = ({ mi_modal, imageUrls, imageLength, actualIndex }) => {
+const ModalHistorias = ({ mi_modal, imageUrls, imageLength, actualIndex, users }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  // const imagenes = useImageStore((state) => state.imageUrls);
-
-  // imageUrls[actualIndex] ? console.log(imageUrls) : ''
-  
   const takeImages = imageUrls
+  const takeUsers = users
+  const defaultImageUrl = 'https://previews.123rf.com/images/pandavector/pandavector1709/pandavector170906212/86095866-persona-%C3%BAnico-icono-de-estilo-de-contorno-persona-ilustraci%C3%B3n-de-stock-de-s%C3%ADmbolo-del-vector.jpg'
 
-  // const tomarImagenes = () => {
-  //   let image = []
+  const allUsers = actualIndex > 0 ? takeUsers.slice(-imageLength) : takeUsers
 
-  //   for (let i of imageUrls) {
-  //     if (i) {
-  //       image.push(...i);
-  //     }
-  //   }
-  //   let totalImage = image.concat(imagenes)
-  //   return totalImage
-  // }
+  let elementKeys = Object.keys(takeImages)
+  let stayElements = elementKeys.slice(actualIndex)
 
-  let info = infoHistorias.slice(actualIndex)
-  imageUrls = imageUrls.slice(actualIndex)
+  const newObject = {};
 
-  let posicionActual = undefined
+  stayElements.forEach(key => {
+    newObject[key] = takeImages[key];
+  });
 
-  const traerPosicionActual = (imageUrl) => {
-    let foundInArray = null;
-
-    for (let i = 0; i < imageUrls.length; i++) {
-      if (imageUrls[i].includes(imageUrl)) {
-        foundInArray = i;
-        break;
-      }
+  let allValues  = []
+  const takeAllValues = () => {
+    for (let x of Object.values(newObject)) {
+      // console.log(x)
+      x.forEach((a) => {
+        allValues.push(a)
+      })
     }
-    posicionActual = foundInArray
   }
+  takeAllValues()
 
   const goToPreviousSlide = () => {
     if (currentIndex > 0) {
@@ -56,35 +45,34 @@ const ModalHistorias = ({ mi_modal, imageUrls, imageLength, actualIndex }) => {
   return (
     <div>
       <dialog className="modal " ref={mi_modal} >
-        <div className="modal-box p-0 overflow-hidden w-2/5 h-full " >
+        <div className="modal-box p-0 bg-transparent overflow-hidden w-2/6 h-full" >
           <div className="modal-action ">
-            <div className=" carousel w-full "  >
-              {takeImages.map((imageUrl, index) => (
+            <div className=" carousel w-full rounded-3xl"  >
+              {allValues.map((imageUrl, index) => (
                 <div
                   key={index}
                   id={`slide${index}`}
-                  className={`carousel-item relative w-full flex flex-col align-center `}
+                  className={`carousel-item relative w-full flex flex-col align-center justify-center`}
                 >
-                  {traerPosicionActual(imageUrl)}
-                  <div>
-                    {info[index] ? (
-                      <div className='flex w-full h-1/2 py-1 absolute nose' >
+                  <div className=''>
+                    {allUsers[index] ? (
+                      <div className='flex w-full h-1/2 absolute nose ' >
                         <img
-                          src={info[posicionActual].imagen_perfil}
+                          src={allUsers[index].profile}
                           alt="Foto de perfil"
-                          className="w-12 h-12 rounded-full ml-1"
+                          className="w-12 h-12 rounded-full ml-1 "
                         />
-                        <p className='mt-3 ml-2 text-slate-100'>{info[posicionActual].nombre}</p>
+                        <p className='mt-3 ml-2 text-slate-100'>{allUsers[index].name}</p>
                       </div>
                     ) : (
                       ''
                     )}
                   </div>
                   <div className='h-full bg-slate-300 '>
-                    <img src={imageUrl} className='object-cover h-full' />
+                    <img src={allUsers[index] ? allUsers[index].storie : defaultImageUrl} className='object-cover h-full w' />
 
                     <div
-                      className={`absolute flex transform -translate-y-1/2 left-5 right-5 top-1/3 justify-between`}
+                      className={`absolute flex transform -translate-y-1/2 left-5 right-5 top-1/3 justify-between `}
                     >
                       <a href={`#slide${index - 1}`} className="btn btn-circle" onClick={goToPreviousSlide}>
                         ❮
