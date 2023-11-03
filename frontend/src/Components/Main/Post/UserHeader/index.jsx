64e7362d27ce31eec96ddf5e785e6df88b3afea8
darklts <dark@merakaistudio.com>
2023-11-03 12:07:02 -0600

    fix avatar img

--- frontend/src/Components/Main/Post/UserHeader/index.jsx
+++ frontend/src/Components/Main/Post/UserHeader/index.jsx
@@ -29,12 +29,12 @@ const UserHeader = ({ avatarUrl, userId, userHandle }) => {
 
   return (
     <div className="user-header dark:bg-neutral-900">
-      <div className="w-1/6">
+      <div className="user-header-avatar">
         <Avatar imageUrl={defaultValue(photoProfile)} />
       </div>
       <div className="text-left">
-        <p className='user-name dark:text-slate-200'>{defaultValue(name)}</p>
-        <span className='user-handle'>@{defaultValue(userName)}</span>
+        <p className="user-name dark:text-slate-200">{defaultValue(name)}</p>
+        <span className="user-handle">@{defaultValue(userName)}</span>
       </div>
     </div>
   )
